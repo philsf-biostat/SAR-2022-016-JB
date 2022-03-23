237749a852dd33b63b8b41e70df3df24d94de137
Felipe Figueiredo <philsf79@gmail.com>
2022-03-23 18:26:43 -0300

    protótipo para compartilhar com a cliente

--- scripts/describe.R
+++ scripts/describe.R
@@ -41,12 +41,12 @@ theme_gtsummary_compact()
 
 # tables ------------------------------------------------------------------
 
-tab_desc <- analytical %>%
-  tbl_summary(
-    include = c(group, outcome),
-    # by = group,
-  ) %>%
-  # modify_caption(caption = "**Tabela 1** Características demográficas") %>%
-  # modify_header(label ~ "**Características dos pacientes**") %>%
-  bold_labels() %>%
-  modify_table_styling(columns = "label", align = "c")
+# tab_desc <- analytical %>%
+#   tbl_summary(
+#     include = c(group, outcome),
+#     # by = group,
+#   ) %>%
+#   # modify_caption(caption = "**Tabela 1** Características demográficas") %>%
+#   # modify_header(label ~ "**Características dos pacientes**") %>%
+#   bold_labels() %>%
+#   modify_table_styling(columns = "label", align = "c")
--- scripts/plots.R
+++ scripts/plots.R
@@ -13,10 +13,33 @@ gg <- analytical %>%
 
 # plots -------------------------------------------------------------------
 
-gg.outcome <- gg +
-  geom_density(aes(outcome, fill = group), alpha = .8) +
-  xlab(attr(analytical$outcome, "label")) +
+# gg.outcome <- 
+gg +
+  geom_density(aes(internacoes, fill = dose), alpha = .8) +
+  xlab(attr(analytical$internacoes, "label")) +
   ylab("")
+gg +
+  geom_line(aes(mes, internacoes)) +
+  xlab(attr(analytical$internacoes, "label")) +
+  ylab("")
+
+gg_d1 <- analytical %>%
+  group_by(ap_resid, mes, fe) %>%
+  filter(dose == "d1") %>%
+  summarise(vacinacao = sum(vacinacao), internacoes = sum(internacoes)) %>%
+  ggplot() +
+  scale_color_brewer(palette = "Paired") +
+  facet_wrap(~ ap_resid, ncol = 2) +
+  labs(subtitle = "Dose 1") +
+  theme_ff()
+
+gg_d1 +
+  geom_line(aes(mes, internacoes, color = fe))
+ggsave("figures/d1_int.png", h = 16, w = 8, units = "cm")
+
+gg_d1 +
+  geom_line(aes(mes, vacinacao, color = fe))
+ggsave("figures/d1_vac.png", h = 16, w = 8, units = "cm")
 
 # cool facet trick from https://stackoverflow.com/questions/3695497 by JWilliman
 # gg +
