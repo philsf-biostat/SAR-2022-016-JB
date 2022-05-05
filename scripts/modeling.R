4a49d993d315c107331ec2b19c6fc83a16ca4de1
Felipe Figueiredo <philsf79@gmail.com>
2022-05-05 20:23:35 -0300

    tabelas de regressão salvas no dir privado (performance)

--- scripts/modeling.R
+++ scripts/modeling.R
@@ -1,6 +1,6 @@
 # setup -------------------------------------------------------------------
 # library(moderndive)
-# library(broom)
+library(broom)
 # library(lmerTest)
 # library(broom.mixed)
 # library(simputation)
@@ -8,6 +8,57 @@
 
 # raw estimate ------------------------------------------------------------
 
+m0 <- lm(internacoes ~ vacinacao,
+         analytical)
 
 # adjusted ----------------------------------------------------------------
 
+m1 <- lm(internacoes ~ vacinacao + ap_resid,
+         analytical)
+m2 <- lm(internacoes ~ vacinacao + fe,
+         analytical)
+m3 <- lm(internacoes ~ vacinacao + dose,
+         analytical)
+m4 <- lm(internacoes ~ vacinacao + ap_resid + fe + dose,
+         analytical)
+
+# report table ------------------------------------------------------------
+
+m0_lab <- "Estimativa bruta"
+m1_lab <- "Controlada por AP"
+m2_lab <- "Controlada por faixa etária"
+m3_lab <- "Controlada por etapa do esquema vacinal"
+m4_lab <- "Modelo final (completo)"
+
+# tab_mod <- tbl_stack(list(
+#   m0 %>% tbl_regression(include = vacinacao, label = list(vacinacao = m0_lab)),
+#   m1 %>% tbl_regression(include = vacinacao, label = list(vacinacao = m1_lab)),
+#   m2 %>% tbl_regression(include = vacinacao, label = list(vacinacao = m2_lab)),
+#   # m3 %>% tbl_regression(include = vacinacao, label = list(vacinacao = m3_lab)),
+#   m4 %>% tbl_regression(include = vacinacao, label = list(vacinacao = m4_lab))
+#   ))
+# 
+# write_rds(tab_mod, "dataset/tab_mod.rds")
+
+# appendix table ----------------------------------------------------------
+
+# tab_app <- tbl_merge(list(
+#   m0 %>% tbl_regression(),
+#   m1 %>% tbl_regression(),
+#   m2 %>% tbl_regression(),
+#   # m3 %>% tbl_regression(),
+#   m4 %>% tbl_regression()
+# ), c(
+#   m0_lab,
+#   m1_lab,
+#   m2_lab,
+#   # m3_lab,
+#   m4_lab
+# ))
+# 
+# write_rds(tab_app, "dataset/tab_app.rds")
+
+# use tables --------------------------------------------------------------
+
+tab_mod <- read_rds("dataset/tab_mod.rds")
+tab_app <- read_rds("dataset/tab_app.rds")
